95f805fbf423b59246a80df1b7734c5da171e320
Rohit Jalan <rohit@purpe.com>
2017-01-25 20:54:30 +0530

    Change to run directory when executing jtagd

--- run/run-jtagd.sh
+++ run/run-jtagd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source `dirname $0`/client-env
-PRELOAD=`dirname $0`/../client-preload-lib/flyserv_client.so
+cd `dirname $0`
+
+#source `dirname $0`/client-env
+PRELOAD=../client-preload-lib/flyserv_client.so
 
 env LD_LIBRARY_PATH=${QUARTUS_ROOTDIR}/linux64 LD_PRELOAD=${PRELOAD} ${QUARTUS_ROOTDIR}/linux64/jtagd
